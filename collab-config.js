95d56dab663153da76853297fdabc41bbf719bc9
serenitybysaule-cpu <serenity.bysaule@gmail.com>
2026-06-02 19:44:30 +0200

    Update collab-config.js

--- collab-config.js
+++ collab-config.js
@@ -1,4 +1,4 @@
 // URL du backend Node.js
 // Local : laisser vide ''
 // Après déploiement Railway : mettre l'URL complète, ex: 'https://serenity-collab.up.railway.app'
-const API_BASE = '';
+const API_BASE = 'https://new-site-production-7e57.up.railway.app';
